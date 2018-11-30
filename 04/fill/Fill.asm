91b08bed3f5ffb9e2b26aed2c32cc00999fc6db7
Anthony Feeney <VigilantNighthawk@gmail.com>
2018-11-30 18:11:59 -0500

    Fixed issues with syntax.  Fixed issues with screen getting colored black when key up.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,60 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+
+//set default color to white
+@color
+M=0
+
+//set to black screen if key pressed
+//otherwise leave color white and draw
+@KBD
+D=M
+@DRAW
+D;JEQ
+@color
+M=-1
+
+(DRAW)
+//initialize rows
+@SCREEN
+D=A
+@32
+D=D+A
+@rows
+M=D
+
+    (FILL)
+    //load color to use
+    @color
+    D=M
+    
+    //color appropriate row
+    @rows   
+    A=M 
+    M=D 
+
+    //continue filling until all 32 rows filled
+    @rows
+    M=M-1
+    @SCREEN
+    D=A
+    @rows
+    D=D-M
+    @FILL
+    D;JLT
+    
+
+
+//restart once filled
+
+@LOOP
+0;JMP
+
+
+
+
+
+
