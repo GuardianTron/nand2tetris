860f39182d254e7d4e95d31ee3026dd3cbcc9a1e
Anthony Feeney <VigilantNighthawk@gmail.com>
2018-12-03 21:45:37 -0500

    Parser is now capable of removing comments and whitespace.

--- 06/assembler/parser.py
+++ 06/assembler/parser.py
@@ -6,3 +6,35 @@ class Parser:
         #pass exception to caller
         with open(path) as f:
             self.file = f.readlines()
+        #initialize the tracking of instruction numbers
+        self.instruction_number=0
+        #used for keeping track of the line being parsed
+        self.line_number=0
+        self.processed = []
+    
+    def parseLine(self):
+        self.line = self.file[self.line_number]
+        self.line_number+=1
+         #strip comments
+        self.stripWhiteSpace()
+        comment = self.line.find("//")
+        if comment==0: #whole line is a comment so skip
+            return
+        elif comment > 0:
+            #remove comment
+            self.line = self.line[0:comment]
+        
+        self.stripWhiteSpace()
+        if len(self.line)==0:    # line is empty
+            return
+        #save processed file for debugging
+        self.processed.append(self.line)
+        
+
+    def parse(self):
+        while self.line_number < len(self.file):
+            self.parseLine()
+
+
+    def stripWhiteSpace(self):
+        self.line = self.line.replace('\n','').replace('\r','').strip()
