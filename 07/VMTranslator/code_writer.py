c642fd6071eeb22047055d4f63f8c0d331b2146f
Anthony Feeney <VigilantNighthawk@gmail.com>
2018-12-09 20:07:19 -0500

    Refactored push writing code in CodeWriter::writePushPop into a private method for clarity.

--- 07/VMTranslator/code_writer.py
+++ 07/VMTranslator/code_writer.py
@@ -37,55 +37,59 @@ class CodeWriter:
         index = int(index)
         asm = []
         if type == Parser.C_PUSH:
-            #handle constant
-            if segment == 'constant':
-                asm.append('@%d'%(index)) #set constant
-                asm.append('D=A') #store constant in register
-                
-            #handle memory segments
-            elif segment in CodeWriter.asm_dynamic_pointers.keys():
-                asm.append("@%s"%(CodeWriter.asm_dynamic_pointers[segment])) #set memory location
-                #instructions common to all     
-                asm.append('D=M') #save location
-                asm.append('@%d'%(index) #set index
-                asm.append('A=D+A') #set pointer
-                asm.append('D=M') #get value out of memory
-            elif segment == 'pointer':
-                if index !='1' or index!='0':
-                    raise new CodeError("push pointer can only take 1 and 0 as arguments.")
-                #get the address of this or that
-                if index == '0':
-                    pointer = "@THIS"
-                else:
-                    pointer = "@THAT"
-                
-                asm.append(pointer)
-                asm.append('D=M') #save current memory address of this
-                asm.append('@%d'%(index))
-                asm.append('D=D+A') #change pointer to this/that + index
-
-            elif segment == 'temp':
-                if index > 7 or index < 0:
-                    raise CodeError("push temp commands must have an index between 0 and 7.")
-                asm.append("@R%d"%(5+index))
-                asm.append('D=M')
-            elif segment == 'static':
-                f_name = self.__current_vm_file
-                asm.append("@%s.%d"%(f_name,index))
-            else: #invalid segement passed
-                raise CodeError("Segment %s is not a valid memory segment."%(segment))
-
-
-                
-            #common instructions to all push commands
-            asm.append('@SP') #set memory location to stack pointer
-            asm.append('A=M') #point to top of stack
-            asm.append('M=D') #set top of stack to new value
-            asm.append('@SP') #set new stack memory location
-            asm.append('M=M+1')
+            asm = self.__writePush(segment,index)
 
         elif type == Parser.C_POP:
             if segment = 'constant':
                 raise CodeError("pop constant is not a legal operation.")
 
-    
+    def __writePush(self,segment,index):
+        asm = []
+        #handle constant
+        if segment == 'constant':
+            asm.append('@%d'%(index)) #set constant
+            asm.append('D=A') #store constant in register
+                
+        #handle memory segments
+        elif segment in CodeWriter.asm_dynamic_pointers.keys():
+            asm.append("@%s"%(CodeWriter.asm_dynamic_pointers[segment])) #set memory location
+            #instructions common to all     
+            asm.append('D=M') #save location
+            asm.append('@%d'%(index) #set index
+            asm.append('A=D+A') #set pointer
+            asm.append('D=M') #get value out of memory
+        elif segment == 'pointer':
+            if index !='1' or index!='0':
+                raise new CodeError("push pointer can only take 1 and 0 as arguments.")
+            #get the address of this or that
+            if index == '0':
+                pointer = "@THIS"
+            else:
+                pointer = "@THAT"
+                
+            asm.append(pointer)
+            asm.append('D=M') #save current memory address of this
+            asm.append('@%d'%(index))
+            asm.append('D=D+A') #change pointer to this/that + index
+
+        elif segment == 'temp':
+            if index > 7 or index < 0:
+                raise CodeError("push temp commands must have an index between 0 and 7.")
+            asm.append("@R%d"%(5+index))
+            asm.append('D=M')
+        elif segment == 'static':
+            f_name = self.__current_vm_file
+            asm.append("@%s.%d"%(f_name,index))
+        else: #invalid segement passed
+            raise CodeError("Segment %s is not a valid memory segment."%(segment))
+
+
+                
+        #common instructions to all push commands
+        asm.append('@SP') #set memory location to stack pointer
+        asm.append('A=M') #point to top of stack
+        asm.append('M=D') #set top of stack to new value
+        asm.append('@SP') #set new stack memory location
+        asm.append('M=M+1')
+
+        return asm
