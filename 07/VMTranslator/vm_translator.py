8b5aa6dd8f3a144e7ee19a7738a2f6d48c81a120
Anthony Feeney <VigilantNighthawk@gmail.com>
2018-12-13 21:58:04 -0500

    Initial implementation of the vm_translator script.

--- 07/VMTranslator/vm_translator.py
+++ 07/VMTranslator/vm_translator.py
@@ -1,7 +1,7 @@
 import sys
 from parser import Parser
-from errors import FileError,ParseError
-
+from code_writer import CodeWriter
+from errors import FileError,ParseError,CodeError
 if(len(sys.argv) < 2):
     print("The translator requires the name of the file to be parsed.")
     exit()
@@ -10,15 +10,26 @@ path = sys.argv[1]
 
 try:
     p = Parser(path)
+    c = CodeWriter(p.path_name)
+    c.setFileName(p.base_name)
     while p.has_more_commands:
         p.advance()
-        info = (p.line_number,p.current_line,p.commandType,p.arg1,p.arg2)
-        print("%d:  %s -- (%s,%s,%s)"%info)
+        
+        if p.commandType == Parser.C_POP or p.commandType == Parser.C_PUSH:
+            c.writePushPop(p.commandType,p.arg1,p.arg2)
+        elif p.commandType == Parser.C_ARITHMETIC:
+            c.writeArithmetic(p.commandType)
+
+    c.close()
+
 
 except FileError as e:
     print(e)
 except ParseError as e:
     print(e)
+except CodeError as e:
+    pe = ParseError(p.line_number,p.current_line,str(e))
+    print(pe)
 except IOError as e:
     errno, strerror = e.args
     print("I/O error({0}): {1}".format(errno,strerror))
