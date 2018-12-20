291cfecc3605d2fd57aa7ea3198f7b12ea0b6fb8
Anthony Feeney <VigilantNighthawk@gmail.com>
2018-12-19 22:55:28 -0500

    Rewrote translator script to utilize the VMTranslator class.

--- 07/VMTranslator/vm_translator.py
+++ 07/VMTranslator/vm_translator.py
@@ -2,42 +2,6 @@ import sys,os
 from parser import Parser
 from code_writer import CodeWriter
 from errors import FileError,ParseError,CodeError
-if(len(sys.argv) < 2):
-    print("The translator requires the name of the file to be parsed.")
-    exit()
-path = sys.argv[1]
-
-
-
-
-try:
-
-
-    p = Parser(path)
-    c = CodeWriter(p.path_name)
-    c.setFileName(p.base_name)
-    while p.has_more_commands:
-        p.advance()
-        
-        if p.commandType == Parser.C_POP or p.commandType == Parser.C_PUSH:
-            c.writePushPop(p.commandType,p.arg1,p.arg2)
-        elif p.commandType == Parser.C_ARITHMETIC:
-            c.writeArithmetic(p.arg1)
-
-    c.close()
-
-
-except FileError as e:
-    print(e)
-except ParseError as e:
-    print(e)
-except CodeError as e:
-    pe = ParseError(p.line_number,p.current_line,str(e))
-    print(pe)
-except IOError as e:
-    errno, strerror = e.args
-    print("I/O error({0}): {1}".format(errno,strerror))
-
 
 class VMTranslator:
 
@@ -90,3 +54,33 @@ class VMTranslator:
 
     def close(self):
         self.__cw.close()
+
+
+if(len(sys.argv) < 2):
+    print("The translator requires the name of the file to be parsed.")
+    exit()
+path = sys.argv[1]
+
+
+
+
+try:
+
+
+    vmt = VMTranslator(path)
+    vmt.translate()
+    vmt.close()
+
+
+except FileError as e:
+    print(e)
+except ParseError as e:
+    print(e)
+except CodeError as e:
+    pe = ParseError(p.line_number,p.current_line,str(e))
+    print(pe)
+except IOError as e:
+    errno, strerror = e.args
+    print("I/O error({0}): {1}".format(errno,strerror))
+
+
