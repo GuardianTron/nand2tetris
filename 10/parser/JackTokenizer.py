5d4c187a5f1e1c31f66e281d7cb137d8296eeeb8
Anthony Feeney <VigilantNighthawk@gmail.com>
2019-01-12 19:46:29 -0500

    Fixed errors in order of parsing rules by adding an array that specifies order. Appears to be error in parsing '/' as comments.

--- 10/parser/JackTokenizer.py
+++ 10/parser/JackTokenizer.py
@@ -3,21 +3,27 @@ import re
 class JackTokenizer:
 
 
-    rules = {
-        "comment":re.compile("(//.*\r?\n)|(/\*.*?\*/)"),
-        "integer":re.compile("\d+"),
-        "identifier":re.compile("[A-Za-z_]\w*"),
-        "string":re.compile("\"*.*?\""),
-        "symbol":re.compile("[{}\(\)\[\]\.,;\+\-\*&\|<>=]"),
-        "keyword":re.compile("(class)|(constructor)|(function)|(method)|(field)|(static)|(var)|(int)|(char)|(boolean)|(void)|(true)|(false)|(null)|(this)|(let)|(do)|(if)|(else)|(while)|(return)"),
-        "whitespace":re.compile("\s+")
-
-
-    }
 
     def __init__(self,filename):
         with open(filename) as fh:
             self.__file = fh.read()
+
+        #ensure rules execute in order
+        self.rule_order = ("comment","integer","keyword","identifier","string","symbol","whitespace")
+
+        self.rules = {}
+        self.rules["comment"]=re.compile("(//.*\r?\n)|(/\*.*?\*/)")
+        self.rules["integer"]=re.compile("\d+")
+        self.rules["keyword"]=re.compile("((class)|(constructor)|(function)|(method)|(field)|(static)|(var)|(int)|(char)|(boolean)|(void)|(true)|(false)|(null)|(this)|(let)|(do)|(if)|(else)|(while)|(return))\s")
+        self.rules["identifier"]=re.compile("[A-Za-z_]\w*")
+        self.rules["string"]=re.compile("\".*?\"")
+        self.rules["symbol"]=re.compile("[{}\(\)\[\]\.,;\+\-\*&\|<>=]") 
+
+        self.rules["whitespace"]=re.compile("\s+")
+        
+        for i in self.rules:
+            print(self.rules[i])
+
 
         #initialize values for scanner
         self.__start = 0
@@ -27,7 +33,7 @@ class JackTokenizer:
     def advance(self):
         current_type = ""
         current_token = ""
-        while self.__start < len(self.__file) and (current_type == "" or current_type == "whitepsace" or current_type == "comment"):
+        while self.__start < len(self.__file) and (current_type == "" or current_type == "whitespace" or current_type == "comment"):
             match = self.__scan_token()
             if not match:
                 raise Exception("No valid token found")
@@ -44,13 +50,25 @@ class JackTokenizer:
     def __scan_token(self):
         match_tuple = None
         
-        for rule in self.__class__.rules.keys():
-            match = re.match(self.__class__.rules[rule],self.__file,self.__start)
+        for rule in self.rule_order:
+            match = self.rules[rule].match(self.__file,self.__start)
             if match:
                 self.__start = match.end() #reset start point for matching
                 match_tuple = (rule,match.group(0))
                 break
         return match_tuple
+
+    def hasMoreTokens(self):
+        return self.__start < len(self.__file)
+
+    
+    @property
+    def type(self):
+        return self.__type
+
+    @property 
+    def raw_token(self):
+        return self.__token
 
 
 
