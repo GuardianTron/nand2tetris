9d01f430b79db65ab689b1bafd5031a6ea973bd8
Anthony Feeney <VigilantNighthawk@gmail.com>
2019-02-21 19:10:58 -0500

    Modified gitignore to not save generated xml files for week 10's project.  Copied over parser code for use in week 11.

---
+++ 11/parser/CompilationEngine.py
@@ -0,0 +1,401 @@
+from JackTokenizer import JackTokenizer,JackTokenizerRewind
+from xml.etree.ElementTree import Element,SubElement
+class CompilationEngine:
+
+    unary_op = {'-','~'} #mathematical and logical negation
+    sub_call_op = {'.','('} #used to determine
+    key_const = {'true','false','null','this'}
+    ops = {'+','-','*','/','&','|','<','>','='}
+
+
+    def xml_decorator(node_name):
+        """ Adds xml generation code to called compilation objects.
+            Manages the node tree for the function, creating a new
+            subnode provided by the name and placing it as the current subnode.
+            Once the decorated function is done executing, the original parent is
+            restored to status of current node.
+            """
+        def decorator(func):
+            def wrapper(self):
+                old_parent = self.__current_parent
+                self.__current_parent = SubElement(old_parent,node_name)
+                func(self)
+                self.__current_parent = old_parent
+            return wrapper
+        return decorator
+
+    def __init__(self,file):
+        self.__file = file
+        self.__tokenizer = JackTokenizerRewind(file)
+        #holds XML root
+        self.__root = None
+        #holds the current parent node
+        self.__current_parent = None
+
+        #bootstrap the compilation process
+        if self.__tokenizer.advance():
+            self.compileClass()
+
+    def getXML(self):
+        return self.__root
+
+    def compileClass(self):
+        self.__root = Element('class')
+        self.__current_parent = self.__root
+        self.__consume(JackTokenizer.KEYWORD,'class')
+        self.__consume(JackTokenizer.IDENTIFIER)
+        self.__consume(JackTokenizer.SYMBOL,'{')
+
+        #process class var declarations
+        while self.__tokenizer.keyword() in ('field','static'):
+            self.compileClassVarDec()
+
+        #process subroutine declarations
+        while  self.__tokenizer.keyword() in ('constructor','method','function'):
+            self.compileSubroutineDec()
+
+
+        self.__consume(JackTokenizer.SYMBOL,'}')    
+
+    @xml_decorator("classVarDec")
+    def compileClassVarDec(self):
+
+        self.__consume(JackTokenizer.KEYWORD,self.__tokenizer.keyword()) #static or field
+
+
+        self.__consumeTypeDec()
+
+        #consume the variable list
+        #make sure their is at least one identifier
+        self.__consume(JackTokenizer.IDENTIFIER)
+        while self.__tokenizer.type == JackTokenizer.SYMBOL and self.__tokenizer.symbol() == ',':
+            self.__consume(JackTokenizer.SYMBOL,',')
+            self.__consume(JackTokenizer.IDENTIFIER)
+
+        #consume ending ';'
+        self.__consume(JackTokenizer.SYMBOL,';')
+
+
+    @xml_decorator("subroutineDec")
+    def compileSubroutineDec(self):
+
+        self.__consume(JackTokenizer.KEYWORD,{'constructor','method','function'})
+
+        #handle return type
+        if self.__tokenizer.type == JackTokenizer.KEYWORD:
+            self.__consume(JackTokenizer.KEYWORD,{'void','int','boolean','char'})
+        else:
+            self.__consume(JackTokenizer.IDENTIFIER)
+
+        #handle function name
+        self.__consume(JackTokenizer.IDENTIFIER)
+
+        #handle expressions
+        self.__consume(JackTokenizer.SYMBOL,'(')
+        self.compileParameterList()
+        self.__consume(JackTokenizer.SYMBOL,')') 
+
+        #move onto subroutineBody
+        self.compileSubroutineBody()       
+
+    @xml_decorator("parameterList")
+    def compileParameterList(self):
+
+        #consume parameter list if any
+        if self.__compileParameter() :
+            #consume additional parameters
+            while self.__tokenizer.token() == ",":
+                self.__consume(JackTokenizer.SYMBOL,',')
+                self.__compileParameter()
+
+    def __compileParameter(self):
+        #if is a parameter, consume the type declaration and the variable name
+        param_keywords = {'int','boolean','char'}
+        if self.__tokenizer.type == JackTokenizer.IDENTIFIER:
+            self.__consume(JackTokenizer.IDENTIFIER)
+            
+        elif self.__tokenizer.type == JackTokenizer.KEYWORD and self.__tokenizer.keyword() in param_keywords:
+            self.__consume(JackTokenizer.KEYWORD,param_keywords)
+        else:  
+            return False
+        self.__consume(JackTokenizer.IDENTIFIER)
+        return True
+
+    @xml_decorator("subroutineBody")
+    def compileSubroutineBody(self):
+
+        self.__consume(JackTokenizer.SYMBOL,'{')
+
+        #handle optional variable declarations.
+        while self.__tokenizer.token() == 'var':
+            self.compileVarDec()  
+
+        self.compileStatements()
+             
+
+        self.__consume(JackTokenizer.SYMBOL,'}')
+
+    @xml_decorator("varDec")
+    def compileVarDec(self):
+
+        self.__consume(JackTokenizer.KEYWORD,'var')
+        self.__consumeTypeDec()
+        self.__consume(JackTokenizer.IDENTIFIER)
+        #handle multiple variables
+        while self.__tokenizer.token() == ',':
+            self.__consume(JackTokenizer.SYMBOL,',')
+            self.__consume(JackTokenizer.IDENTIFIER)
+
+        #finish declaration
+        self.__consume(JackTokenizer.SYMBOL,';')
+
+    @xml_decorator("statements")
+    def compileStatements(self):
+
+        while self.__tokenizer.type == JackTokenizer.KEYWORD and self.__tokenizer.keyword() in ('do','let','if','return','while'):
+            token = self.__tokenizer.keyword()
+            if token == 'do':
+                self.compileDo()
+            elif token == 'let':
+                self.compileLet()
+            elif token == 'if':
+                self.compileIf()
+            elif token == 'return':
+                self.compileReturn()
+            elif token == 'while':
+                self.compileWhile()
+
+    @xml_decorator("doStatement")
+    def compileDo(self):
+        self.__consume(JackTokenizer.KEYWORD,"do")
+        self.compileSubroutineCall()
+        self.__consume(JackTokenizer.SYMBOL,";")
+    
+    @xml_decorator("letStatement")
+    def compileLet(self):
+        """compiles let statements."""
+
+        self.__consume(JackTokenizer.KEYWORD,'let')
+        self.compileVariable()
+        self.__consume(JackTokenizer.SYMBOL,"=")
+        self.compileExpression()
+        self.__consume(JackTokenizer.SYMBOL,";")
+
+    @xml_decorator("ifStatement")
+    def compileIf(self):
+
+        self.__consume(JackTokenizer.KEYWORD,'if')
+        self.__consume(JackTokenizer.SYMBOL,'(')
+        self.compileExpression()
+        self.__consume(JackTokenizer.SYMBOL,')')
+        self.__consume(JackTokenizer.SYMBOL,'{')
+        self.compileStatements()
+        self.__consume(JackTokenizer.SYMBOL,"}")
+
+        if self.__tokenizer.type == JackTokenizer.KEYWORD and self.__tokenizer.keyword() == 'else':
+            self.__consume(JackTokenizer.KEYWORD,'else')
+            self.__consume(JackTokenizer.SYMBOL,'{')
+            self.compileStatements()
+            self.__consume(JackTokenizer.SYMBOL,'}')
+
+    @xml_decorator("returnStatement")
+    def compileReturn(self):
+        self.__consume(JackTokenizer.KEYWORD,'return')
+        #not an emptry return, so compile expression
+        if not (self.__tokenizer.type == JackTokenizer.SYMBOL and self.__tokenizer.symbol() == ';'):
+            self.compileExpression()
+        self.__consume(JackTokenizer.SYMBOL,';')
+
+    @xml_decorator("whileStatement")
+    def compileWhile(self):
+        self.__consume(JackTokenizer.KEYWORD,'while')
+        self.__consume(JackTokenizer.SYMBOL,'(')
+        self.compileExpression()
+        self.__consume(JackTokenizer.SYMBOL,')')
+        self.__consume(JackTokenizer.SYMBOL,'{')
+        self.compileStatements()
+        self.__consume(JackTokenizer.SYMBOL,'}')
+
+    @xml_decorator("expressionList")
+    def compileExpressionList(self):
+        """Compiles a list of expressions."""
+        #utilizes the fact that all expression lists are currently contained within parenthesis to test
+        if not (self.__tokenizer.type == JackTokenizer.SYMBOL and self.__tokenizer.symbol() == ')'):
+            self.compileExpression()
+            while self.__tokenizer.type == JackTokenizer.SYMBOL and self.__tokenizer.symbol() == ',':
+                self.__consume(JackTokenizer.SYMBOL,',')
+                self.compileExpression()
+    
+    @xml_decorator("expression")
+    def compileExpression(self):
+        self.compileTerm()
+        while self.__tokenizer.token() in self.ops:
+            self.__consume(JackTokenizer.SYMBOL,self.ops)
+            self.compileTerm()
+
+    @xml_decorator("term")
+    def compileTerm(self):
+        """Compiles individual terms. Terms can be recursively defined"""
+        t_type = self.__tokenizer.type
+        if t_type == JackTokenizer.INT:
+            self.__consume(JackTokenizer.INT)
+        elif t_type == JackTokenizer.STRING:
+            self.__consume(JackTokenizer.STRING)
+        elif t_type == JackTokenizer.KEYWORD and self.__tokenizer.keyword() in self.key_const: #true,false,null,this
+            #consume the keyword, but save in case it is part of a method call on 'this'
+            token = self.__tokenizer.keyword()
+            #obtain next token for testing if it is a method call on this
+            self.__tokenizer.advance()
+            next_token = self.__tokenizer.token()
+            #restore keyword token for later consumption
+            self.__tokenizer.rewind()
+            
+            if token == 'this' and  next_token == '.': #method call
+                self.compileSubroutineCall()
+            else: #single keyword
+                self.__consume(JackTokenizer.KEYWORD,self.key_const)
+
+        elif t_type == JackTokenizer.SYMBOL and self.__tokenizer.symbol() == '(':   #assume parenthesis by itself starts an expression
+            self.__consume(JackTokenizer.SYMBOL,'(')
+            self.compileExpression()
+            self.__consume(JackTokenizer.SYMBOL,')')
+        elif t_type == JackTokenizer.SYMBOL and self.__tokenizer.symbol() in self.unary_op: # example mathematical negation of an expression -- call term again
+            self.__consume(JackTokenizer.SYMBOL,self.unary_op)
+            self.compileTerm()
+        elif t_type == JackTokenizer.IDENTIFIER: #can be a variable, array, or function call
+            self.__tokenizer.advance() #if a variable, rest of conditionals will fall through
+            t_type = self.__tokenizer.type
+            token = self.__tokenizer.token()
+            self.__tokenizer.rewind() #rewind back to identifier for processing methods to consume
+            if t_type == JackTokenizer.SYMBOL and token in self.sub_call_op: #handle method and function calls
+               self.compileSubroutineCall()
+            else: #if not a method call, assume variable or array
+                self.compileVariable()
+
+    def compileSubroutineCall(self):
+        """
+           Handles parsing of the subroutine call
+        """
+        t_type = self.__tokenizer.type
+        if t_type == JackTokenizer.KEYWORD and self.__tokenizer.keyword() == 'this': #handle this identifier
+            self.__consume(JackTokenizer.KEYWORD,'this')
+        else: #assume an identifier
+            self.__consume(JackTokenizer.IDENTIFIER)
+
+
+        if self.__tokenizer.token() == '.': #if method call, consume . and method name identifier
+            self.__consume(JackTokenizer.SYMBOL,'.')
+            self.__consume(JackTokenizer.IDENTIFIER)
+        #handle The code was developed against Pi 3 B, I have not tested on Pi 1. I think references to the name BCM2835 may be accidental, perhaps more accurate would be to call it BCM2837, although also possible that they sharethe actual function call portion -- Always runs
+        self.__consume(JackTokenizer.SYMBOL,'(')
+        self.compileExpressionList()
+        self.__consume(JackTokenizer.SYMBOL,')')
+
+    def compileVariable(self):
+        """Compile a variable and array declaration."""
+        self.__consume(JackTokenizer.IDENTIFIER)
+        t_type = self.__tokenizer.type
+        if t_type == JackTokenizer.SYMBOL and self.__tokenizer.symbol() == '[': #arrays
+            self.__consume(JackTokenizer.SYMBOL,'[')
+            self.compileExpression()
+            self.__consume(JackTokenizer.SYMBOL,']')
+
+
+
+
+    
+
+
+    #note: token is passed for checking that the token matches a specific
+    #expected value.  The token recorded is taken from the tokenizer
+    #thus, only pre-enumerated tokens such as symbols or keywords will use the 
+    #token paramenter. 
+    #token can either be a single token or an array of possible values
+    def __consume(self,t_type,token=None):
+        #test type
+        if self.__tokenizer.type != t_type:
+            raise CompilationError("%s  -- Expecting type: %s  Received type: %s"%(self.__file,t_type,self.__tokenizer.type))
+        
+        #if specific token(s) 
+        if token:
+            #if not a list or set, then wrap in set for comparison
+            if not isinstance(token,(list,set)):
+                token = {token}
+            if self.__tokenizer.token() not in token:
+                
+                raise CompilationError("{} -- Expecting {} of type {}. Received {}: {}".format(self.__file,token,t_type, self.__tokenizer.type,self.__tokenizer.token()))
+
+
+        #generate xml for token
+        token_xml = SubElement(self.__current_parent,self.__tokenizer.type)
+        token_xml.text = " {} ".format(str(self.__tokenizer.token()))
+        self.__tokenizer.advance()
+
+    #helper method for consuming type declarations
+    def __consumeTypeDec(self):
+        t_type = self.__tokenizer.type
+
+        #varable type can be a keyword constant or class name
+        if t_type == JackTokenizer.KEYWORD:
+            self.__consume(JackTokenizer.KEYWORD,{'int','char','boolean'})
+        else:
+            self.__consume(JackTokenizer.IDENTIFIER)
+
+
+
+
+class CompilationError(Exception):
+    pass    
+
+class NotValidJackFileError(Exception):
+    pass
+
+import os
+def get_jack_files(filename):
+    """ Returns a generator that enumerate all jack files in a directory.
+        Returns a generator with the jack file is the path is a jack file.
+        Throws NoValidJackFile if no jack files found.
+    """
+    if os.path.isfile(filename):
+        if comp_extension(filename,"jack"):
+            yield filename
+        else:
+            raise NotValidJackFileError(filename)
+    elif os.path.isdir(filename):
+        #loop through files and return jack files
+        constains_jack_file = False #flags if no jack files found
+        dirs = os.listdir(filename)
+        for file in dirs:
+            file = os.path.join(filename,file)
+            if os.path.isfile(file) and comp_extension(file,"jack"):
+                constains_jack_file = True
+                yield file
+
+        if not constains_jack_file:
+            raise NotValidJackFileError(filename)
+    else:
+        raise NotValidJackFileError(filename)
+
+def comp_extension(filename,extension):
+    return os.path.basename(filename).split(".")[-1] == extension
+
+def create_xml_path(filename):
+    directory = os.path.dirname(filename)
+    file = os.path.basename(filename).split(".")[0]
+    xml_file = file+"_output.xml"
+    return os.path.join(directory,xml_file)
+
+if __name__ == "__main__":
+    from sys import argv
+    from xml.etree import ElementTree as ET
+    try:
+        for file in get_jack_files(argv[1]):
+            compiler = CompilationEngine(file)
+            xml = compiler.getXML()
+            with open(create_xml_path(file),'w') as doc:
+                doc.write(ET.tostring(xml,'unicode'))     
+
+    except IOError as e:
+        print(e)
+    except NotValidJackFileError as e:
+        print(e)
---
+++ 11/parser/JackTokenizer.py
@@ -0,0 +1,187 @@
+import re
+
+class JackTokenizer:
+
+    KEYWORD = "keyword"
+    INT = "integerConstant"
+    IDENTIFIER = "identifier"
+    STRING = "stringConstant"
+    SYMBOL = "symbol"
+    WHITESPACE = "whitespace"
+    COMMENT = "comment"
+
+    keywords = ["class","constructor","function","method","field","static","var","int","char","boolean","void","true","false","null","this","let","do","if","else","while","return"]
+
+
+    def __init__(self,filename):
+        with open(filename) as fh:
+            self.__file = fh.read()
+
+
+
+        #ensure rules execute in order
+        self.__rule_order = (self.COMMENT,self.INT,self.KEYWORD,self.IDENTIFIER,self.STRING,self.SYMBOL,self.WHITESPACE)
+
+        #create the keyword regex
+        key_regex = "(%s)\W"%("|".join(["(%s)"%(key) for key in self.keywords]))
+        self.__rules = {}
+        self.__rules[self.COMMENT]=re.compile("(//.*?\r?\n)|(/\*.*?(\*/))",re.DOTALL)
+        self.__rules[self.INT]=re.compile("\d+")
+        self.__rules[self.KEYWORD]=re.compile(key_regex)
+        self.__rules[self.IDENTIFIER]=re.compile("[A-Za-z_]\w*")
+        self.__rules[self.STRING]=re.compile("\".*?\"")
+        self.__rules[self.SYMBOL]=re.compile("[{}\(\)\[\]\.,;\+\-\*&\|<>=/~]") 
+        self.__rules[self.WHITESPACE]=re.compile("\s+")
+
+
+        #initialize values for scanner
+        self.__start = 0
+        self.__type = ""
+        self.__token = None
+
+    def advance(self):
+        current_type = ""
+        current_token = ""
+        while self.__start < len(self.__file) and (current_type == "" or current_type == self.WHITESPACE or current_type == self.COMMENT):
+            match = self.__scan_token()
+            if not match:
+                raise Exception("No valid token found\n"+self.__file[self.__start:])
+            current_type = match[0]
+            current_token =  match[1]
+
+
+        self.__type = current_type
+        self.__token = current_token
+
+        return self.__start < len(self.__file) and (current_type != self.WHITESPACE and current_type != self.COMMENT)
+
+
+
+    
+    def __scan_token(self):
+        match_tuple = None
+        
+        for rule in self.__rule_order:
+            match = self.__rules[rule].match(self.__file,self.__start)
+            if match:
+               
+                self.__start = match.end() #reset start point for matching
+                match_string = match.group(0)
+                if rule == self.KEYWORD:
+                     #Keywords will consume an extra non alphanumeric/underscore character
+                    #Remove this extra character and handle the matches appropriately
+                    self.__start -= 1
+                    match_string = match_string[:-1]
+                match_tuple = (rule,match_string)
+                break
+        return match_tuple
+
+    def symbol(self):
+        return self.__token
+
+    def keyword(self):
+        return self.__token.strip()
+
+    def identifier(self):
+        return self.__token
+
+    def string(self):
+        return self.__token[1:-1]
+
+    def integer(self):
+        return int(self.__token)
+
+    #convience method for returing the token
+    def token(self):
+        if self.__type == JackTokenizer.INT:
+            return self.integer()
+        elif self.__type == JackTokenizer.STRING:
+            return self.string()
+        elif self.__type == JackTokenizer.KEYWORD:
+            return self.keyword()
+        else:
+            return self.raw_token
+    
+    @property
+    def type(self):
+        return self.__type
+
+    @type.setter
+    def type(self,t_type):
+        self.__type = t_type
+
+    @property 
+    def raw_token(self):
+        return self.__token
+
+    @raw_token.setter
+    def raw_token(self,token):
+        self.__token = token
+
+
+class JackTokenizerRewind(JackTokenizer):
+    """Tokenizes Jack program files. 
+        Adds ability to move backward 
+        and forwad through the token stream."""
+
+    def __init__(self,filename):
+        super().__init__(filename)
+        self.__tokens = []
+        self.__index = -1
+
+    def advance(self):
+        "Advance to the next token"
+        #revisiting already scanned tokens
+        if self.__index < len(self.__tokens) - 1:
+            self.__index+=1
+            #set the current token for return
+            self.type,self.raw_token = self.__tokens[self.__index]
+            return True
+        elif super().advance():
+                self.__index = len(self.__tokens)
+                self.__tokens.append((self.type,self.raw_token))
+                return True
+        return False
+    
+    def rewind(self):
+        "Move backwards through stack. Returns true if not at bottom"
+        if self.__index > 0:
+            self.__index -= 1
+            #set current token
+            self.type, self.raw_token = self.__tokens[self.__index]
+            return True
+        return False
+                
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import os.path
+    from xml.etree.ElementTree import Element,SubElement,ElementTree
+    from xml.etree import ElementTree as ET
+    try:
+        f_name = argv[1]
+        tokenizer = JackTokenizer(argv[1])
+        root = Element('tokens')
+        while tokenizer.advance():
+            token_element = SubElement(root,tokenizer.type)
+            token_element.text = " "+str(tokenizer.token())+" "
+
+        #save the xml output
+        base_name = os.path.basename(f_name).split(".")[0]
+        xml_name = os.path.join(os.path.dirname(f_name),base_name+"T_test.xml")
+        with open(xml_name,'w') as doc:
+            doc.write(ET.tostring(root,'unicode'))
+    except Exception as e:
+        print(e)
+        raise e
+
+        
+
+
+
+
+        
+        
+
+
